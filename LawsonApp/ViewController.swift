490c57297ded12269e0ac13b7aaac8f6bcda24df
Alexey <swiftslayeer@gmail.com>
2024-07-03 20:02:29 +0300

    Start

--- LawsonApp/ViewController.swift
+++ LawsonApp/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  LawsonApp
-//
-//  Created by Алексей Шишкарев on 03.07.2024.
-//
 
 import UIKit
 
@@ -11,7 +5,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .gray
     }
 
 
